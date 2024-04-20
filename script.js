31997c26089932ee4ba2a224e4c74f43557d8c02
eimaryy <mnery0809@gmail.com>
2024-04-20 11:16:55 -0300

    Footer: mobile finalizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuBt = document.querySelector('.menu');
 const menuAberto = document.querySelector('#header_menu-aberto');
-const menuSair = document.querySelector('.header_menu-sair')
+const menuSair = document.querySelector('.header_menu-sair');
 
 menuBt.addEventListener('click', () => {
     menuAberto.style.display = 'flex'
@@ -8,3 +8,35 @@ menuBt.addEventListener('click', () => {
 menuSair.addEventListener('click', () => {
     menuAberto.style.display = 'none'
 });
+
+const btnCompra = document.querySelector('.com');
+const btnAtendimento = document.querySelector('.at');
+const btnSobre = document.querySelector('.sob');
+const ulCompra = document.querySelector('.comprar');
+const ulAtendimento = document.querySelector('.atendimento');
+const ulSobre = document.querySelector('.sobre');
+
+btnCompra.addEventListener('click', () =>{
+    let textBtn = ['Comprar +', 'Comprar -']
+    eventoClicker(ulCompra, btnCompra, textBtn);
+});
+
+btnAtendimento.addEventListener('click', () =>{
+    let textBtn = ['Atendimento ao Cliente +', 'Atendimento ao Cliente -']
+    eventoClicker(ulAtendimento, btnAtendimento, textBtn);
+});
+
+btnSobre.addEventListener('click', () =>{
+    let textBtn = ['Sobre +', 'Sobre -']
+    eventoClicker(ulSobre, btnSobre, textBtn);
+});
+
+function eventoClicker(ul, btn, textBtn){
+if (ul.style.display == 'none'){
+    ul.style.display = 'flex'
+    btn.innerHTML = textBtn[1]
+} else{
+    ul.style.display = 'none'
+    btn.innerHTML = textBtn[0]
+}
+}
